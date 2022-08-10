7f8c3e1ed73cbbf6039c0a8481f22a34b8d75347
sabirkhanit <mskhan98@gmail.com>
2022-08-10 12:00:47 +0530

    initial code

---
+++ src/main/java/com/schema/tenancy/SchemaTenancyApp.java
@@ -0,0 +1,14 @@
+package com.schema.tenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SchemaTenancyApp {
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaTenancyApp.class);
+
+	    }
+}
